6a99607e7c79e10f6b08f59f29e3837e106713d7
federico-peruzzo <f302.federico@gmail.com>
2024-02-20 14:03:53 +0100

    WS2812 Testing - Prima prova [NOT WORKING]
    Iniziato test sui ProgrammableIO:
     - Test build con pioasm
     - Ricerca utilizzo FIFO di output

--- testing/test_ws2812/main.cpp
+++ testing/test_ws2812/main.cpp
@@ -1,18 +1,15 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  while(!Serial);
+  delay(2000);
+  Serial.println("WS2812 Test code started...");
+  Serial.print("CPU Clock Frequency: ");
+  Serial.print(rp2040.f_cpu() / 1000000.0);
+  Serial.println(" MHz");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(200);
 }
