db19dd4c92ed80fafe881139a8644837e802d947
Technorico <f302.federico@gmail.com>
2024-02-26 20:27:45 +0100

    Correzione e perfezionamento classe Bussola
     - Integrazione calibrazione magnetometro/Bussola
     - Modifica sistema calibrazione, aggiunta di due funzioni di calibrazione. Una per l'accelerometro e una per il magnetometro
     - Aggiunto "Rolling Rounding" ai dati raccolti.

--- include/Bussola.h
+++ include/Bussola.h
@@ -1,8 +1,11 @@
 #ifndef __BUSSOLA_H__
 #define __BUSSOLA_H__
-#define CALIBRATION_SAMPLES 300
+#define ACCELEROMETER_CALIBRATION_SAMPLES 500
+#define MAGNETOMETER_CALIBRATION_SAMPLES 800
 #define CALIBRATION_TIME_STEP 10
-#define PRE_CALIBRATION_WAIT 10
+#define PRE_ACCELEROMETER_CALIBRATION_WAIT 1000
+#define N_SAMPLES_ACCELEROMETER_AVERAGING 25
+#define N_SAMPLES_MAGNETOMETER_AVERAGING 25
 
 #include <Wire.h>
 #include <Arduino.h>
@@ -27,15 +30,18 @@ enum CalibrationStatus_t {
 
 enum FSMState_t {
     UPDATING_DATA,
-    CALIBRATION,
+    CALIBRATION_ACCELEROMETER,
+    CALIBRATION_MAGNETOMETER
 };
 
 class Bussola {
   private:
     struct CalStruct_t {
         Axis_t accelerometer_offset;
-        float magnetometer_degree_offset;
+        Axis_t magnetometer_offset, magnetometer_min, magnetometer_max;
         CalibrationStatus_t calibrationStatus;
+        CalibrationStatus_t accelerometerCalibrationStatus;
+        CalibrationStatus_t magnetometerCalibrationStatus;
     };
     Adafruit_LSM303_Accel_Unified _acc;
     Adafruit_LSM303_Mag_Unified _mag;
@@ -43,10 +49,15 @@ class Bussola {
     FSMState_t state, state_pre;
     unsigned long long pre_calibration_wait_endTime, calibration_time_step_endTime;
     unsigned int calibration_step;
-    float magnetometer_degree;
-    Axis_t accelerometer;
+    unsigned int magnetometer_last_value_index;
+    unsigned int accelerometer_last_value_index;
+    float magnetometer_degree[N_SAMPLES_MAGNETOMETER_AVERAGING], magnetometer_degree_averaged;
+    Axis_t accelerometer[N_SAMPLES_ACCELEROMETER_AVERAGING], accelerometer_value_averaged;
     bool classInitialized;
-    bool drdy, drdy_pre;
+    bool accelerometer_rolling_rounding_active;
+    bool magnetometer_rolling_rounding_active;
+    bool accelerometer_averaging_valid;
+    bool magnetometer_averaging_valid;
 
     float getDegree(float mag_x, float mag_y, float mag_z);
     bool isStateEntry();
@@ -54,12 +65,17 @@ class Bussola {
 
   public:
     Bussola(Adafruit_LSM303_Accel_Unified& _acc = acc, Adafruit_LSM303_Mag_Unified& _mag = mag);
-    void calibrate_sensor();
-    void stopCalibration();
+    void calibrate_accelerometer();
+    void calibrate_magnetometer();
+    void stopResetCalibration();
     CalibrationStatus_t getCalibrationStatus();
+    CalibrationStatus_t getAccelerometerCalibrationStatus();
+    CalibrationStatus_t getMagnetometerCalibrationStatus();
     bool init();
     bool isClassInitialized();
     void update();
+    void accelerometerRollingRoundig(bool activate);
+    void magnetometerRollingRoundig(bool activate);
     float getDegree();
     Axis_t getAccelerometerData();
 };
--- testing/test_mpu9250/main.cpp
+++ testing/test_mpu9250/main.cpp
@@ -1,11 +1,17 @@
 #include "PSEBoard.h"
 #include "Bussola.h"
 
+#define LED_WAIT_MS 400
+
 Bussola bus = Bussola();
-unsigned long long t1;
+unsigned long long t1, t_led;
+bool led = HIGH;
+float degree_pre = 0.0, degree = 0.0;
 
 void setup(void)
 {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, led);
     Wire.setSDA(I2C0_SDA);
     Wire.setSCL(I2C0_SCL);
     Serial.begin(115200);
@@ -14,32 +20,49 @@ void setup(void)
     Serial.println("Accelerometer and Compass Test"); Serial.println("");
     Serial.print("Is the class Bussola Initialized? "); Serial.println(bus.isClassInitialized());
     bus.init();
+    bus.accelerometerRollingRoundig(true);
+    bus.magnetometerRollingRoundig(true);
     Serial.print("Is the class Bussola Initialized? "); Serial.println(bus.isClassInitialized());
     Serial.print("Is sensor Calibrated? (yes = 2) "); Serial.println(bus.getCalibrationStatus());
-    bus.calibrate_sensor();
+    bus.calibrate_accelerometer();
     Serial.print("Is sensor Calibrated? (yes = 2) "); Serial.println(bus.getCalibrationStatus());
-    t1 = millis() + 500;
+    t1 = millis() + 1000;
+    t_led = millis();
 }
 
 void loop(void)
 {
-    if(bus.getCalibrationStatus() == CalibrationStatus_t::CALIBRATION_DONE) {
-        if(millis() >= t1) {
-            Serial.print("Is sensor Calibrated? (yes = 2) "); Serial.println(bus.getCalibrationStatus());
-            Axis_t accData = bus.getAccelerometerData();
-            Serial.print("X: "); Serial.print(accData.x); Serial.print("  ");
-            Serial.print("Y: "); Serial.print(accData.y); Serial.print("  ");
-            Serial.print("Z: "); Serial.print(accData.z); Serial.print("  "); Serial.println("m/s^2 ");
-            Serial.print("Angle: "); Serial.print(bus.getDegree()); Serial.println(" Degree");
-            Serial.println();
-            t1 = millis() + 500;
+    if(bus.getAccelerometerCalibrationStatus() == CALIBRATION_DONE) {
+        bus.calibrate_magnetometer();
+        if(bus.getCalibrationStatus() == CALIBRATION_DONE) {
+            if(millis() >= t1) {
+                Serial.print("Is sensor Calibrated? (yes = 2) "); Serial.println(bus.getCalibrationStatus());
+                Serial.print("Is accelerometer Calibrated? (yes = 2) "); Serial.println(bus.getAccelerometerCalibrationStatus());
+                Serial.print("Is magnetometer Calibrated? (yes = 2) "); Serial.println(bus.getMagnetometerCalibrationStatus());
+                Axis_t accData = bus.getAccelerometerData();
+                Serial.print("X: "); Serial.print(accData.x); Serial.print("  ");
+                Serial.print("Y: "); Serial.print(accData.y); Serial.print("  ");
+                Serial.print("Z: "); Serial.print(accData.z); Serial.print("  "); Serial.println("m/s^2 ");
+                //if(degree < (degree_pre - 5.0) || degree > (degree_pre + 5.0)) {
+                    Serial.print("Angle: "); Serial.print(degree); Serial.println(" Degree");
+                    degree_pre = degree;
+                //}
+                degree = bus.getDegree();
+                Serial.println();
+                t1 = millis() + 1000;
+            }
         }
     }
-    else {
-        if(millis() >= t1) {
-            Serial.print("Is sensor Calibrated? (yes = 2) "); Serial.println(bus.getCalibrationStatus());
-            t1 = millis() + 500;
-        }
+    if(millis() >= t1 && bus.getCalibrationStatus() != CALIBRATION_DONE) {
+        Serial.print("Is sensor Calibrated? (yes = 2) "); Serial.println(bus.getCalibrationStatus());
+        Serial.print("Is accelerometer Calibrated? (yes = 2) "); Serial.println(bus.getAccelerometerCalibrationStatus());
+        Serial.print("Is magnetometer Calibrated? (yes = 2) "); Serial.println(bus.getMagnetometerCalibrationStatus());
+        t1 = millis() + 1000;
+    }
+    if(millis() >= t_led) {
+        led = !led;
+        digitalWrite(LED_BUILTIN, led);
+        t_led = millis() + LED_WAIT_MS;
     }
     bus.update();
 }
