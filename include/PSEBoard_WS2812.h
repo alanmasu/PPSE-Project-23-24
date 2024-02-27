9b1d7e5078b51df7e377b409a4ae02ac16df920d
Technorico <f302.federico@gmail.com>
2024-02-27 16:29:20 +0100

    Implemetazione gestione led WS2812
     - Spostata implemetazione su una classe
     - Testate tutte le chiamate a funzione
     - Stare attenti alla funzione di 'ledShowCalibration' che deve essere chiamata regolarmente

---
+++ include/PSEBoard_WS2812.h
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <PSEBoard.h>
+#include <Adafruit_NeoPixel.h>
+
+#define LED_COUNT 8
+#define DEFAULT_BRIGHTNESS 50
+#define ANGLE_STEP (360.0 / LED_COUNT)
+#define HALF_ANGLE_STEP (ANGLE_STEP / 2.0)
+#define MAGNETOMETER_CALIBRATION_LED_TIME 250
+
+const uint32_t LED_RED = Adafruit_NeoPixel::Color(255, 0, 0);
+const uint32_t LED_GREEN = Adafruit_NeoPixel::Color(0, 255, 0);
+const uint32_t LED_VIOLET = Adafruit_NeoPixel::Color(200, 0, 255);
+const uint32_t LED_OFF = Adafruit_NeoPixel::Color(0, 0, 0);
+
+class PSEBoard_WS2812 {
+    public:
+        void init();
+        void globalBrightness(uint8_t brightness);
+        void ledShowDirection(float angle);
+        void ledShowFix(bool fix);
+        void ledShowCalibration(bool calibrated, bool magnetometer_calibrating = false);
+        void ledOffShow();
+        void ledFill(uint32_t color, bool show = false);
+
+    private:
+        void ledOff(bool show = false);
+        bool blink;
+        unsigned long long t_magnetometer_calibration_led;
+        Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, WS2812_PIN, NEO_GRB + NEO_KHZ800);
+};
--- testing/test_ws2812/main.cpp
+++ testing/test_ws2812/main.cpp
@@ -1,117 +1,58 @@
 #include <Arduino.h>
 #include <PSEBoard.h>
 #include <Adafruit_NeoPixel.h>
+#include <PSEBoard_WS2812.h>
 
-#define LED_COUNT 8
-#define BRIGHTNESS 50
-#define ANGLE_STEP (360.0 / LED_COUNT)
-#define HALF_ANGLE_STEP (ANGLE_STEP / 2.0)
-#define MAGNETOMETER_CALIBRATION_LED_TIME 500
+unsigned long long t1, t2;
 
-Adafruit_NeoPixel strip(LED_COUNT, WS2812_PIN, NEO_GRB + NEO_KHZ800);
-const uint32_t LED_RED = strip.Color(255, 0, 0);
-const uint32_t LED_GREEN = strip.Color(0, 255, 0);
-const uint32_t LED_VIOLET = strip.Color(200, 0, 255);
-const uint32_t LED_OFF = strip.Color(0, 0, 0);
-
-bool blink = false;
-unsigned long long t_magnetometer_calibration_led = 0, t1 = 0;
-
-void setWS2812(float angle) {
-  strip.fill(LED_OFF, 0, LED_COUNT);
-  if(angle > HALF_ANGLE_STEP*15 || angle <= HALF_ANGLE_STEP) {
-    strip.setPixelColor(7, LED_VIOLET);
-    strip.setPixelColor(0, LED_VIOLET);
-  }
-  else if(angle > HALF_ANGLE_STEP && angle <= HALF_ANGLE_STEP*3) {
-    strip.setPixelColor(0, LED_VIOLET);
-    strip.setPixelColor(1, LED_VIOLET);
-  }
-  else if(angle > HALF_ANGLE_STEP*3 && angle <= HALF_ANGLE_STEP*5) {
-    strip.setPixelColor(1, LED_VIOLET);
-    strip.setPixelColor(2, LED_VIOLET);
-  }
-  else if(angle > HALF_ANGLE_STEP*5 && angle <= HALF_ANGLE_STEP*7) {
-    strip.setPixelColor(2, LED_VIOLET);
-    strip.setPixelColor(3, LED_VIOLET);
-  }
-  else if(angle > HALF_ANGLE_STEP*7 && angle <= HALF_ANGLE_STEP*9) {
-    strip.setPixelColor(3, LED_VIOLET);
-    strip.setPixelColor(4, LED_VIOLET);
-  }
-  else if(angle > HALF_ANGLE_STEP*9 && angle <= HALF_ANGLE_STEP*11) {
-    strip.setPixelColor(4, LED_VIOLET);
-    strip.setPixelColor(5, LED_VIOLET);
-  }
-  else if(angle > HALF_ANGLE_STEP*11 && angle <= HALF_ANGLE_STEP*13) {
-    strip.setPixelColor(5, LED_VIOLET);
-    strip.setPixelColor(6, LED_VIOLET);
-  }
-  else if(angle > HALF_ANGLE_STEP*13 && angle <= HALF_ANGLE_STEP*15) {
-    strip.setPixelColor(6, LED_VIOLET);
-    strip.setPixelColor(7, LED_VIOLET);
-  }
-  strip.show();
-}
-
-void ledShowFix(bool fix) {
-  if(fix) {
-    strip.fill(LED_GREEN, 0, LED_COUNT);
-  } else {
-    strip.fill(LED_RED, 0, LED_COUNT);
-  }
-  strip.show();
-}
-
-void ledShowCalibration(bool calibrated, bool magnetometer_calibrating) {
-  if(millis() >= t_magnetometer_calibration_led) {
-    blink = !blink;
-    t_magnetometer_calibration_led = millis() + MAGNETOMETER_CALIBRATION_LED_TIME;
-  }
-  if(calibrated) {
-    strip.fill(LED_GREEN, 0, LED_COUNT);
-  } else {
-    if(magnetometer_calibrating) {
-      if(blink) {
-        strip.fill(LED_RED, 0, LED_COUNT);
-      } else {
-        strip.fill(LED_OFF, 0, LED_COUNT);
-      }
-    } else {
-      strip.fill(LED_RED, 0, LED_COUNT);
-    }
-  }
-  strip.show();
-}
+PSEBoard_WS2812 pseboard;
 
 void setup() {
   Serial.begin(115200);
-
   while(!Serial);
-  delay(2000);
-  Serial.println("WS2812 Test code started...");
-  strip.begin();
-  strip.show();
-  strip.setBrightness(BRIGHTNESS);
-  t_magnetometer_calibration_led = millis();
-  t1 = millis() + 5000;
-  ledShowCalibration(true, false);
+  delay(1000);
+  pseboard.init();
+  t1 = millis() + 3000;
+  t2 = millis() + 10000;
   delay(2000);
 }
 
 bool fix = false;
+bool all_calibrated = false;
+bool magnetometer_calibrating = false;
+bool disable1 = false;
+bool disable2 = false;
 
 void loop() {
-  /*for(unsigned int i = 0; i <= 360; i++) {
-    setWS2812((float)i);
-    delay(10);
-    Serial.print("Angle: ");
-    Serial.print(i);
-    Serial.println(" degree");
-  }*/
-  if(millis() >= t1) {
-    ledShowCalibration(false, true);
-  } else {
-    ledShowCalibration(false, false);
+  if(millis() < t1 && !disable1) {
+    for(unsigned int i = 0; i <= 360; i++) {
+      pseboard.ledShowDirection((float)i);
+      delay(10);
+    }
+  }
+  if(millis() >= t1 && !disable2) {
+    disable1 = true;
+    pseboard.ledOffShow();
+    pseboard.ledShowFix(fix);
+    if(!fix) {
+      t1 = millis() + 2000;
+      fix = !fix;
+    }
+  }
+  if(disable1 && disable2) {
+    pseboard.ledShowCalibration(all_calibrated, magnetometer_calibrating);
+  }
+  if(millis() >= t2) {
+    disable2 = true;
+    if(!all_calibrated && !magnetometer_calibrating) {
+      magnetometer_calibrating = true;
+      t2 = millis() + 4000;
+    }
+    else {
+      if(!all_calibrated && magnetometer_calibrating) {
+        all_calibrated = true;
+        t2 = millis() + 2000;
+      }
+    }
   }
 }
